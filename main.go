8aef241a9c346d93b5d7f12634f9f52d02f6c21a
Taichi Masuyama <montanha.masu536@gmail.com>
2020-07-18 02:19:50 +0900

    updated

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import(
-	"fmt"
+	// "fmt"
+	"html/template"
 	"log"
 	"io/ioutil"
 	"net/http"
@@ -27,16 +28,51 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// URL.Pathは例えば /view/testの部分を取ってくる
 	title := r.URL.Path[len("/view/"):] // /view/以降の文字列をとる
-	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s<h1><div>%s</div>", p.Title, p.Body)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	// fmt.Fprintf(w, "<h1>%s<h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	// URL.Pathは例えば /edit/testの部分を取ってくる
+	title := r.URL.Path[len("/edit/"):] // /edit/以降の文字列をとる
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	// fmt.Fprintf(w, "<h1>%s<h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body") // htmlの方でtextareaのname属性を"body"にして、submitで送信したため、"body"で中身をとることができる
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
 	p1 := &Page{Title: "test", Body: []byte("This is a simple Page")}
 	p1.save()
 	http.HandleFunc("/view/", viewHandler) // "/view/"の場合
-	log.Fatal(http.ListenAndServe(":8080", nil)) // "/"でアクセスしたときにこれが呼ばれる nilの部分はdefault handler となり、page not foundをかえせる
+	http.HandleFunc("/edit/", editHandler) // "/edit/"の場合
+	http.HandleFunc("/save/", saveHandler) // "/save/"の場合
+	log.Fatal(http.ListenAndServe(":8080", nil)) // "/"でアクセスしたときにこれが呼ばれる nilの部分はdefault handler となり、page not foundを返す
 }
